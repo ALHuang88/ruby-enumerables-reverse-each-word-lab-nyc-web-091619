32acd862557ce8375837f987bdea7ff14ef6a92d
Allen Huang <alhuang1288@gmail.com>
2019-08-28 16:54:33 +0000

    Automatically backed up by Learn

---
+++ reverse_each_word.rb
@@ -0,0 +1,6 @@
+def reverse_each_word(sentence)
+new_sent = sentence.split
+reverse = new_sent.each {|s| s.reverse!}
+return reverse.join(" ")
+end
+p reverse_each_word("Hello there, and how are you?")
